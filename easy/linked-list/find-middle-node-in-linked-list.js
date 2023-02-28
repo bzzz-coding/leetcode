61c0b8eb6b0b9f8986c732030f8f2c9f1e7634a0
bzzz-coding <86077274+bzzz-coding@users.noreply.github.com>
2023-02-28 10:47:49 -0800

    Added find-middle-node-of-linked-list solution and updated README

--- easy/linked-list/find-middle-node-in-linked-list.js
+++ easy/linked-list/find-middle-node-in-linked-list.js
@@ -1,8 +1,36 @@
-# leetcode
+/*
+Given the head of a linked list, return the middle node of the linked list.
 
-## Linked Lists
+If there are two middle nodes, return the second middle node.
 
-### Finding the middle of a linked list
+https://leetcode.com/problems/middle-of-the-linked-list
+*/
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+var middleNode = function (head) {
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return slow
+};
+
+
+/*
 - Concept: Nodes do not have an index, but a `next` property. 
 - Hint: Loop through all the nodes at two speeds `.next` and `.next.next`. By the time the second speed reaches the end, the first will be at the middle. Return what node the first speed is currently at.
 - Solution:
@@ -11,3 +39,4 @@
       - `slow` moves at 1x speed, to `slow.next`
       - `fast` moves at 2x speed, to `fast.next.next` 
   - When `fast` reaches the end of the linked list (`fast.next === null`), `slow` is at the middle of the list
+*/
