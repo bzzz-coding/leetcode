e7a037d1117b1b806ee0eac85c25fe9a454c620f
bzzz-coding <86077274+bzzz-coding@users.noreply.github.com>
2023-04-24 14:51:05 -0700

    Rewrote function

--- easy/linked-list/reverse-linked-list.js
+++ easy/linked-list/reverse-linked-list.js
@@ -16,16 +16,33 @@ https://leetcode.com/problems/reverse-linked-list/
  * @param {ListNode} head
  * @return {ListNode}
  */
-var reverseList = function(head) {
-  if (head === null) return head
-  let current = head;
-  let previous = null;
-  while (current !== null) {
-    // let temp = current.next;
-    // current.next = previous;
-    // previous = current;
-    // current = temp;
-    [current.next, previous, current] = [previous, current, current.next]
+
+// go through the list, break it into reversed and remaining, the head of the remaining will have a next that points to the reversed, until there is no remaining
+
+function reverseList(head) {
+  if (!head || !head.next) return head
+  let remaining = head.next;
+  let reversed = head;
+  reversed.next = null;
+  while (remaining) {
+    [reversed, reversed.next, remaining] = [remaining, reversed, remaining.next]
   }
-  return previous
-};
+  // if no remaining, return reversed
+  return reversed
+}
+
+
+
+// var reverseList = function(head) {
+//   if (head === null) return head
+//   let current = head;
+//   let previous = null;
+//   while (current !== null) {
+//     // let temp = current.next;
+//     // current.next = previous;
+//     // previous = current;
+//     // current = temp;
+//     [current.next, previous, current] = [previous, current, current.next]
+//   }
+//   return previous
+// };
