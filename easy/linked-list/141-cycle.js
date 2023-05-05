e47d2bfa39adc97bca2eadbd427ba41b6adef200
bzzz-coding <86077274+bzzz-coding@users.noreply.github.com>
2023-05-05 12:03:27 -0700

    Added solution

---
+++ easy/linked-list/141-cycle.js
@@ -0,0 +1,40 @@
+/*
+
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+
+Constraints:
+
+The number of the nodes in the list is in the range [0, 104].
+-105 <= Node.val <= 105
+pos is -1 or a valid index in the linked-list.
+
+https://leetcode.com/problems/linked-list-cycle/
+*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+const hasCycle = (head) => {
+  if (!head) return false;
+  let stackSet = new Set([]);
+  let currentNode = head;
+  while (currentNode) {
+    if (stackSet.has(currentNode)) return true;
+    stackSet.add(currentNode);
+    currentNode = currentNode.next;
+  }
+  return false
+};  
